0d5440cf55a127bc84a2031180a4b47dae183c9c
ruets <dev@ruets.pro>
2023-03-16 11:30:50 +0100

    adding dynamic title and price

--- src/controller/stock_controller.js
+++ src/controller/stock_controller.js
@@ -1,20 +1,19 @@
 "use strict";
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+const urlParams = new URLSearchParams(window.location.search);
+let companyData = {
+  symbol: urlParams.get("symbol"),
+  name: urlParams.get("name"),
+};
 
-const stock = urlParams.get("stock");
-document.title = stock + " - Stock";
+const stocks = getStockValue(companyData.symbol);
 
-const stocks = getStockValue(stock);
-const data = stocks["Meta Data"];
+document.title = companyData.symbol + " - " + companyData.name;
+stockView.title.innerHTML = companyData.symbol + " - " + companyData.name;
 
-console.log(stockView.titleStock);
+stocks.then(function (data) {
+  let timeSeries = data["Time Series (5min)"];
+  let lastRefreshed = data["Meta Data"]["3. Last Refreshed"];
 
-//Getting overview and fundamental information
-const companyOverview = getCompanyOverview(stock).then((companyOverview) => {
-    stockView.titleStock.textContent = companyOverview.Name;
+  stockView.price[0].innerHTML = timeSeries[lastRefreshed]["4. close"];
 });
-
-
-console.log(stocks);
